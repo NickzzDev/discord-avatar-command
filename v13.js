6978e64c4af51200a489a7dbe381677a459ab3b6
Nickzz <85542797+NickzzDev@users.noreply.github.com>
2021-09-26 18:18:29 +1000

    Create v13.js

---
+++ v13.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+exports.run = async(client, message, args) => {
+
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+
+    const embed = new Discord.MessageEmbed()
+        .setFooter(`PFP requested by ${message.author.tag}`)
+        .setColor(`0x00f8ff`)
+        .setTimestamp()
+        .setTitle(`${member.user.tag} Profile Picture`)
+        .setDescription(`[View Avatar Here](${member.user.displayAvatarURL({ dynamic: true, size: 4096 })}) \n\nMade by [Nickzz](https://github.com/NickzzDev/)`)
+        .setImage(member.user.displayAvatarURL({ dynamic: true }))
+        message.channel.send({ embeds: [embed] }) 
+
+}
